2ad8e4e8681f5b78711a49abf9097075bd4b2679
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-03-02 16:43:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -38,9 +38,9 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sumOfPrime = 0;
-  for (int i = 2; i < hbound; i++) {
+  for (int i = 2; i <= hbound; i++) {
       if (checkPrime(i) == true) {
-          sumOfPrime += 1;
+          sumOfPrime += i;
       }
   }
   return sumOfPrime;
